98ce8d1cd25a743d75cc9e90df42e5e8b2ef91ec
maikyy24 <108490561+maikyy24@users.noreply.github.com>
2026-04-04 14:13:52 -0600

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0 
+
+        def dfs(node):
+            nonlocal diameter
+            if node is None:
+                return 0
+            left = dfs(node.left) 
+            right = dfs(node.right)
+            diameter = max(diameter, right + left) 
+            return 1 + max(left, right)
+
+        dfs(root)
+        return diameter
